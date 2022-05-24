90a3a2cbdad8db5bb6d4402ce14548b92c4ad710
Nahuel61920 <estebancarrizo619@gmail.com>
2022-05-24 03:50:55 -0300

    End 01-IntroToCs

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,26 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  let sumDecimal = 0
+  let array = num.split("");
 
+  for (let i = 0; i < array.length; i++) {
+    sumDecimal = sumDecimal + array[i] * 2 ** (array.length - 1 - i);
+  }
+  return sumDecimal;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
 
+  let binario = ""
+
+  while (num > 0) {
+    binario = (num % 2) + binario;
+    num = Math.floor(num / 2)
+  }
+
+  return binario
 }
 
 
